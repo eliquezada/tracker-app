aac2dc57a83789162bbe5e0b748ce21f0518a2c4
eli quezada <elizabeth.quezada1@gmail.com>
2020-05-03 13:26:46 +0200

    added Dashboard Component

---
+++ resources/js/components/DashboardComponent.vue
@@ -0,0 +1,25 @@
+<template>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <div class="card">
+                    <div class="card-header">Dashboard</div>
+                    <div class="card-body">
+                        Dashboard
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+</template>
+
+<script>
+    export default {
+        mounted() {
+        }
+    }
+</script>
+
+<style scoped>
+
+</style>
