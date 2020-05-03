e9e769143f58ec3b8af6f46e22301b498cab0dc3
eli quezada <elizabeth.quezada1@gmail.com>
2020-05-03 11:14:34 +0200

    updated mc

--- app/Http/Controllers/TaskController.php
+++ app/Http/Controllers/TaskController.php
@@ -2,9 +2,17 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
+use App\Models\Task;
 
 class TaskController extends Controller
 {
-    //
+    public function __construct()
+    {
+        $this->middleware('auth');
+    }
+
+    public function index()
+    {
+        return Task::mine()->get()->toArray();
+    }
 }
--- app/Models/Task.php
+++ app/Models/Task.php
@@ -6,5 +6,26 @@ use Illuminate\Database\Eloquent\Model;
 
 class Task extends Model
 {
-    //
+    protected $fillable = [
+        'name', 'user_id', 'stopped_at', 'started_at'
+    ];
+
+    protected $dates = ['started_at', 'stopped_at'];
+
+    protected $with = ['user'];
+
+    public function user()
+    {
+        return $this->belongsTo(User::class);
+    }
+
+    public function scopeMine($query)
+    {
+        return $query->whereUserId(auth()->user()->id);
+    }
+
+    public function scopeRunning($query)
+    {
+        return $query->whereNull('stopped_at');
+    }
 }
