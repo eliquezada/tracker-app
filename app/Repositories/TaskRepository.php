b63a879df8613490fa70f33fab2e77eab4baa7a6
eli quezada <elizabeth.quezada1@gmail.com>
2020-05-05 20:27:55 +0200

    added taskrepo

---
+++ app/Repositories/TaskRepository.php
@@ -0,0 +1,78 @@
+<?php
+
+namespace App\Repositories;
+
+use App\Models\Task;
+
+use Illuminate\Http\Request;
+use Carbon\Carbon;
+
+class TaskRepository
+{
+
+    /**
+     * Get all task by user
+     *
+     * @return array
+     */
+    public function getAll()
+    {
+        return Task::mine()->idDescending()->get()->toArray();
+    }
+
+    /**
+     * Create & store a task
+     *
+     * @param $request
+     *
+     * @return array
+     */
+    public function create(Request $request)
+    {
+        $data = $request->validate(['name' => 'required|between:3,25']);
+
+        $data = array_merge($data, ['user_id' => auth()->user()->id,'started_at' => new Carbon]);
+
+        $task = Task::create($data);
+
+        return  $task->toArray();
+    }
+
+    /**
+     * Update a task
+     *
+     * @param $id
+     *
+     * @return array
+     */
+    public function update($id)
+    {
+        $task = Task::find($id);
+        $task->stopped_at = new Carbon;
+        $task->save();
+
+        return $task;
+    }
+
+    /**
+     * Find a task by id
+     *
+     * @param $id
+     *
+     * @return mixed
+     */
+    public function find($id)
+    {
+        return Task::find($id);
+    }
+
+    /**
+     * Find user active task
+     *
+     * @return mixed
+     */
+    public function running()
+    {
+        return Task::mine()->running()->first();
+    }
+}
