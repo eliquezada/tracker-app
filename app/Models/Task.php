8b6525bba9325c487dfce47df7437b6bab07ffaf
eli quezada <elizabeth.quezada1@gmail.com>
2020-05-05 20:37:37 +0200

    added model functions scope

--- app/Models/Task.php
+++ app/Models/Task.php
@@ -12,20 +12,58 @@ class Task extends Model
 
     protected $dates = ['started_at', 'stopped_at'];
 
-    protected $with = ['user'];
+     /**
+     * access to user
+     * @return mixed
+     */
 
     public function user()
     {
         return $this->belongsTo(User::class);
     }
 
+    /**
+     * Scope a query to search by login user
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
     public function scopeMine($query)
     {
         return $query->whereUserId(auth()->user()->id);
     }
 
+    /**
+     * Scope a query to get active tasks
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+
     public function scopeRunning($query)
     {
         return $query->whereNull('stopped_at');
+    }
+
+    /**
+     * Scope a query to only include popular users.
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeIdDescending($query)
+    {
+        return $query->orderByDesc('id');
+    }
+
+    /**
+     * Scope a query to group by task name
+     *
+     * @param  \Illuminate\Database\Eloquent\Builder  $query
+     * @return \Illuminate\Database\Eloquent\Builder
+     */
+    public function scopeGroupName($query)
+    {
+        return $query->groupByRaw('name');
     }
 }
